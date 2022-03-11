ad8e954c3ea83b652d7f4160e52c5d6d2f4ca381
Caio Costa <hcaio370@gmail.com>
2022-03-10 22:56:27 -0300

    Update exercicio7.java

--- exercicio7.java
+++ exercicio7.java
@@ -1,19 +1,21 @@
+package lista1;
+
 import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    Scanner valores = new Scanner(System.in);
-    int idade, meses, dias, ttidade, ttmeses, totalgeral;
-    System.out.println("Informe sua idade: ");
-    idade = valores.nextInt();
-    System.out.println("Informe os meses: ");
-    meses = valores.nextInt();
-    System.out.println("Informe os dias: ");
-    dias = valores.nextInt();
-    ttidade = idade * 365;
-    ttmeses = meses * 30;
-    totalgeral = ttidade + ttmeses + dias;
-    valores.close();
-    System.out.println("Idade expressa em dias: " + totalgeral);
-  }
+public class Exercicio7 {
+	public static void main(String[] args) {
+		Scanner valores = new Scanner(System.in);
+		int idade, meses, dias, ttidade, ttmeses, totalgeral;
+		System.out.println("Informe sua idade: ");
+		idade = Integer.parseInt(valores.nextLine());
+		System.out.println("Informe os meses: ");
+		meses = Integer.parseInt(valores.nextLine());
+		System.out.println("Informe os dias: ");
+		dias = Integer.parseInt(valores.nextLine());
+		ttidade = idade * 365;
+		ttmeses = meses * 30;
+		totalgeral = ttidade + ttmeses + dias;
+		valores.close();
+		System.out.println("Idade expressa em dias: " + totalgeral);
+	}
 }
