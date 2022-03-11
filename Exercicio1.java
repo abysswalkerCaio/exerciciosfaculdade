ea6977637a09d6da02abf27d03799cc5e8824bfd
Caio Costa <hcaio370@gmail.com>
2022-03-10 22:50:30 -0300

    Update Exercicio1.java

--- Exercicio1.java
+++ Exercicio1.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 import java.text.DecimalFormat;
 
 public class Exercicio1 {
-  private static final DecimalFormat df = new DecimalFormat("0.00");
-  public static void main (String [] args) {
-    Scanner valores = new Scanner(System.in);
-      double cotacao, valordolar, resultadoreal;
-      System.out.println("Insira a cotação do dólar: ");
-      cotacao = Double.parseDouble(valores.nextLine());
-      System.out.println("Insira o valor em dólar: ");
-      valordolar = Double.parseDouble(valores.nextLine());
-      resultadoreal = cotacao * valordolar;
-      valores.close();
-      System.out.println("O valor em real: " + df.format(resultadoreal);
-    }
+	private static final DecimalFormat df = new DecimalFormat("0.00");
+
+	public static void main(String[] args) {
+		Scanner valores = new Scanner(System.in);
+		double cotacao, valordolar, resultadoreal;
+		System.out.println("Insira a cotação do dólar: ");
+		cotacao = Double.parseDouble(valores.nextLine());
+		System.out.println("Insira o valor em dólar: ");
+		valordolar = Double.parseDouble(valores.nextLine());
+		resultadoreal = cotacao * valordolar;
+		valores.close();
+		System.out.println("O valor em real: R$ " + df.format(resultadoreal));
+	}
 }
