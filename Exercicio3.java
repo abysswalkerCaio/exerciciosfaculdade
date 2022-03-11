a61400aadf47c522fcab72f6912a3d3bb2455c8c
Caio Costa <hcaio370@gmail.com>
2022-03-10 22:38:56 -0300

    Update and rename exercicio3.java to Exercicio3.java

---
+++ Exercicio3.java
@@ -0,0 +1,20 @@
+package lista1;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Exercicio3 {
+	private static final DecimalFormat df = new DecimalFormat("0.00");
+	public static void main(String[] args) {
+	    Scanner valores = new Scanner(System.in);
+	    double unidade, totalvendas, calculoporc, pagamentocomissao;
+	    System.out.println("Valor total de uma unidade: ");
+	    unidade = Double.parseDouble(valores.nextLine());
+	    System.out.println("Total de unidades vendidas: ");
+	    totalvendas = Double.parseDouble(valores.nextLine());
+	    calculoporc = unidade + totalvendas;
+	    pagamentocomissao = calculoporc * 0.05;
+	    valores.close();
+	    System.out.println("Sua comissão: R$ " + df.format(pagamentocomissao));
+	  }
+}
