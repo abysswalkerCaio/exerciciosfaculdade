45783f6058f2768e21891790925c1f7d267d9055
Caio Costa <hcaio370@gmail.com>
2022-03-10 22:52:13 -0300

    Update Exercicio4.java

--- Exercicio4.java
+++ Exercicio4.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Exercicio4 {
 	public static void main(String[] args) {
-	    Scanner valores = new Scanner(System.in);
-	    int numero, antecessor;
-	    System.out.println("Digite um número: ");
-	    numero = Integer.parseInt(valores.nextLine());
-	    antecessor = numero - 1;
-	    valores.close();
-	    System.out.println("O antecessor do valor inserido é: " + antecessor);
-	  }
+		Scanner valores = new Scanner(System.in);
+		int numero, antecessor;
+		System.out.println("Digite um número: ");
+		numero = Integer.parseInt(valores.nextLine());
+		antecessor = numero - 1;
+		valores.close();
+		System.out.println("O antecessor do valor inserido é: " + antecessor);
+	}
 }
