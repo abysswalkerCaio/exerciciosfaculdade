c78fead3b1a7d98ae36974b0333df78c7e975c84
Caio Costa <hcaio370@gmail.com>
2022-03-26 01:24:05 -0300

    Update Exercicio6.java

--- lista1/Exercicio6.java
+++ lista1/Exercicio6.java
@@ -5,11 +5,16 @@ import java.util.Scanner;
 public class Exercicio6 {
 	public static void main(String[] args) {
 		Scanner valores = new Scanner(System.in);
+		
 		int idade, dias;
+		
 		System.out.println("Informe sua idade: ");
 		idade = Integer.parseInt(valores.nextLine());
+		
 		dias = idade * 365;
+		
 		valores.close();
+		
 		System.out.println("Idade expressa em dias: " + dias);
 	}
 }
