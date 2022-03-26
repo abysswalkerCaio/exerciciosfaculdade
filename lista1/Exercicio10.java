f6ec8d5593c49c5e7b42035e3b9304253e9805ba
Caio Costa <hcaio370@gmail.com>
2022-03-26 01:35:05 -0300

    Update Exercicio10.java

--- lista1/Exercicio10.java
+++ lista1/Exercicio10.java
@@ -4,17 +4,21 @@ import java.util.Scanner;
 import java.text.DecimalFormat;
 
 public class Exercicio10 {
-	private static final DecimalFormat df = new DecimalFormat("0.00");
-
 	public static void main(String[] args) {
+		DecimalFormat df = new DecimalFormat("0.00");
 		Scanner valores = new Scanner(System.in);
+		
 		double custo, imposto, distribuidor, custofinal;
+		
 		System.out.println("Informe o custo de fábrica: ");
 		custo = Double.parseDouble(valores.nextLine());
+		
 		imposto = custo * 0.45;
 		distribuidor = custo * 0.28;
 		custofinal = custo + imposto + distribuidor;
+		
 		valores.close();
+		
 		System.out.println("O custo final do carro: " + df.format(custofinal));
 	}
 }
