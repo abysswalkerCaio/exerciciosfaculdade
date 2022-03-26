5378f7d6eba5f7fab4845a78d74219577c075f5a
Caio Costa <hcaio370@gmail.com>
2022-03-26 01:33:45 -0300

    Update Exercicio9.java

--- lista1/Exercicio9.java
+++ lista1/Exercicio9.java
@@ -4,18 +4,22 @@ import java.util.Scanner;
 import java.text.DecimalFormat;
 
 public class Exercicio9 {
-	private static final DecimalFormat df = new DecimalFormat("0.00");
-
 	public static void main(String[] args) {
+		DecimalFormat df = new DecimalFormat("0.00");
 		Scanner valores = new Scanner(System.in);
+		
 		double salario, reajuste, valor, valorreajuste;
 		System.out.println("Informe o salário: ");
 		salario = Double.parseDouble(valores.nextLine());
+		
 		System.out.println("Informe o percentual de reajuste: ");
 		reajuste = Double.parseDouble(valores.nextLine());
+		
 		valor = salario * (reajuste / 100);
 		valorreajuste = valor + salario;
+		
 		valores.close();
+		
 		System.out.println("Valor reajustado: R$ " + df.format(valorreajuste));
 	}
 }
