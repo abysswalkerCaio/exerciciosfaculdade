04430e4c9be9ce14247ad25a5d3916d6ebddef6e
Caio Costa <hcaio370@gmail.com>
2022-03-26 01:19:04 -0300

    Update Exercicio3.java

--- lista1/Exercicio3.java
+++ lista1/Exercicio3.java
@@ -4,18 +4,23 @@ import java.util.Scanner;
 import java.text.DecimalFormat;
 
 public class Exercicio3 {
-	private static final DecimalFormat df = new DecimalFormat("0.00");
-
 	public static void main(String[] args) {
+		DecimalFormat df = new DecimalFormat("0.00");
 		Scanner valores = new Scanner(System.in);
+
 		double unidade, totalvendas, calculoporc, pagamentocomissao;
-		System.out.println("Valor total de uma unidade: ");
+
+		System.out.print("Valor total de uma unidade: ");
 		unidade = Double.parseDouble(valores.nextLine());
-		System.out.println("Total de unidades vendidas: ");
+
+		System.out.print("Total de unidades vendidas: ");
 		totalvendas = Double.parseDouble(valores.nextLine());
+		
 		calculoporc = unidade + totalvendas;
 		pagamentocomissao = calculoporc * 0.05;
+		
 		valores.close();
+		
 		System.out.println("Sua comissão: R$ " + df.format(pagamentocomissao));
 	}
 }
